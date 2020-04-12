661d429c57f70277bfc5f3aaf860119177f84087
Stefan VanBuren <vanburenstefan@gmail.com>
2020-04-11 21:58:12 -0400

    fish: Drop fisher

--- config.fish
+++ config.fish
@@ -1,10 +1,3 @@
-# bootstrap fisher
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 # enable fish's vi key bindings
 fish_vi_key_bindings
 
