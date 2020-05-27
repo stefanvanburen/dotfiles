3cdda609f64f819375e2be7abd822e2cb5f130b4
Stefan VanBuren <vanburenstefan@gmail.com>
2020-05-27 18:48:31 -0400

    fish: Add back pyenv / direnv hooks

--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
 if status --is-interactive
     command -q jump; and source (jump shell fish | psub)
     command -q starship; and starship init fish | source
+
+    command -q pyenv; and source (pyenv init -|psub); and source (pyenv virtualenv-init -|psub)
+    command -q direnv; and direnv hook fish | source
 end
