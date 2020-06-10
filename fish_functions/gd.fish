1cb0160b4bdd7727e7c763f03700f4d190b1494b
Stefan VanBuren <vanburenstefan@gmail.com>
2020-06-09 22:46:30 -0400

    fish: Remove gd and gs functions

--- fish_functions/gd.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function gd --wraps='git diff' --description 'alias gd git diff'
-  git diff $argv;
-end
--- fish_functions/gs.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function gs --wraps='git status' --wraps='git st' --description 'alias gs git st'
-  git st $argv;
-end
