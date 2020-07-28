f6398575d85490c2d07394b1f7250eaaa6211209
Stefan VanBuren <vanburenstefan@gmail.com>
2020-07-28 13:00:32 -0400

    fish: jump -> z, j -> just

--- config.fish
+++ config.fish
@@ -1,10 +1,9 @@
-alias gd 'git diff'
-alias gs 'git st'
 alias x extract
 alias ls exa
 alias cat bat
 alias git hub
 alias c clear
+alias j just
 
 alias vim nvim
 alias vimrc 'vim ~/.config/nvim/init.vim'
@@ -13,7 +12,8 @@ alias gv 'vim +G +only'
 alias vw 'vim -c "cd ~/nv" -c VimwikiIndex -c Rg'
 
 if status --is-interactive
-    command -q jump; and source (jump shell fish | psub)
+    # jump is bound to `z`
+    command -q jump; and source (jump shell --bind=z fish | psub)
     command -q starship; and starship init fish | source
 
     command -q direnv; and direnv hook fish | source
