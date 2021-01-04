5cef7e94f4631a2ddf966dd02b6cc9f5969a7b5e
Stefan VanBuren <me@svbn.me>
2021-01-04 13:58:32 -0500

    fish: Fix slow tab completion

---
+++ fish_functions/__fish_describe_command.fish
@@ -0,0 +1,5 @@
+function __fish_describe_command --description 'Command used to find descriptions for commands'
+# This slows down tab completion.
+# empty on purpose, see
+# https://github.com/fish-shell/fish-shell/issues/6270
+end
