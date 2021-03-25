5e0b545c4e02e47f2497d953801f3a4ca4de416f
Stefan VanBuren <me@svbn.me>
2021-03-25 13:02:02 -0400

    fish: Remove workaround for slow tab completion
    Should be fixed by [1].
    
    [1]: https://github.com/fish-shell/fish-shell/pull/7365

--- fish_functions/__fish_describe_command.fish
+++
@@ -1,5 +0,0 @@
-function __fish_describe_command --description 'Command used to find descriptions for commands'
-# This slows down tab completion.
-# empty on purpose, see
-# https://github.com/fish-shell/fish-shell/issues/6270
-end
