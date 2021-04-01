5da4e4362c12afd2b4b67e814726aeae84ed3180
Stefan VanBuren <me@svbn.me>
2021-04-01 10:25:54 -0400

    fish: Remove diff function
    Not really using this!

--- fish_functions/diff.fish
+++
@@ -1,9 +0,0 @@
-function diff
-    # If not exactly two arguments, write out usage
-    if test (count $argv) -ne 2
-        echo "Usage: diff file1 file2" >&2
-        return 1
-    end
-
-    git diff --no-index $argv[1] $argv[2]
-end
