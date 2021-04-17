73155ea12cc20b3c0f91f930a6e760de517805d0
Stefan VanBuren <stefan@vanburen.xyz>
2021-04-16 21:38:58 -0400

    nvim: svanburen -> stefanvanburen

--- nvim/init.lua
+++ nvim/init.lua
@@ -130,7 +130,7 @@ require('packer').startup(function(use)
   use 'tpope/vim-abolish'
 
   -- colorscheme
-  use {'svanburen/rams.vim', branch = 'lsp' }
+  use {'stefanvanburen/rams.vim', branch = 'lsp' }
 
 end)
 
