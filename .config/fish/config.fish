ad3f4f504a7b64e07e3e271d043e0186ec68e517
Stefan VanBuren <svanburen@buf.build>
2023-10-29 10:46:01 -0400

    fish: Remove `extract` function
    I don't really use this - I mostly just extra things in a GUI, now. (I
    don't deal with many compressed files in the first place, it seems.)
    
    Somewhat reluctant to do this because I [blogged about it][1], but no
    need to keep it around!
    
    [1]: https://stefan.vanburen.xyz/blog/fish-extract/

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -59,7 +59,6 @@ if status --is-interactive
     abbr --add --global g git
     abbr --add --global b buf
     abbr --add --global m make
-    abbr --add --global x extract
     abbr --add --global c clear
     abbr --add --global j just
     abbr --add --global rd rmdir
